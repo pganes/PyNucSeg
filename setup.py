d23ca6ad0f7b2b53ebf530d2676974abbce44d37
pganes <gpande3@uic.edu>
2023-03-03 23:34:12 -0600

    example file added

--- setup.py
+++ setup.py
@@ -5,21 +5,25 @@ with open("README.md","r") as fh:
 
 setup(
     name='PyNucSeg',
-    version='0.0.1',
-    description='A python package for segmentation of fluorescent nucleus, and measure the area of nucleus and the mean intensity per nucleus. This package uses stardist(https://github.com/stardist/stardist) for segmentation and napari(https://github.com/napari/napari) for visualization.',
-    py_modules=['pynucseg'],
-    package_dir={'':'pynucseg'},
-    classifiers=[
-    "Programming Language :: Python :: 3.10",
-    ],
+    version='0.0.7',
+    author="Ganesh Pandey",
+    author_email="gpande3@uic.edu",
+    description='Segment fluorescent nucleus, and measure the area, and the mean intensity per nucleus.',
     long_description=long_description,
     long_description_content_type="text/markdown", 
+    url="https://github.com/pganes/PyNucSeg",
+    packages=['pynucseg'],
+    package_dir={'': 'src'},
+    classifiers=[
+        "Programming Language :: Python :: 3.10",
+    ],
     install_requires=[
     "numpy",
     "stardist",
     "napari",
     "scikit-image",
     "tensorflow",
+    "pyqt5"
     ],
     extras_require ={
     "dev": [
@@ -27,8 +31,6 @@ setup(
     "twine",
     ]
     },
-    url="https://github.com/pganes/PyNucSeg",
-    author="Ganesh Pandey",
-    author_email="gpande3@uic.edu",
+    
 
 )
